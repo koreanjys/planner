1323687cad13654afb67790e2ee840a840559425
koreanjys <connecttojys@gmail.com>
2023-12-15 04:41:15 +0900

    user routes, models

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routes.users import user_router
+
+import uvicorn
+
+app = FastAPI()
+
+# 라우트 등록
+app.include_router(user_router, prefix="/user")
+
+# uvicorn 앱 실행
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ models/users.py
@@ -0,0 +1,35 @@
+# models/users.py
+
+from pydantic import BaseModel, EmailStr
+from typing import List, Optional
+from models.events import Event
+
+class User(BaseModel):
+    email: EmailStr  # email 형식에 맞는지 검사
+    password: str
+    username: str
+    events: Optional[List[Event]] = []  # Optioanl을 사용할 때 None 값이라도 미리 정해주지 않으면 에러가 발생한다. 그러니 = None 이라도 값을 주자
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "username": "FastPackt"
+            }
+        }
+    }
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+            }
+        }
+    }
+
