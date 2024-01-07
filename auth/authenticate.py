06f4417262e881067ab4af14e70efb6ec995ae83
koreanjys <connecttojys@gmail.com>
2024-01-07 12:08:01 +0900

    JWT ing..

--- auth/authenticate.py
+++ auth/authenticate.py
@@ -1,4 +1,31 @@
 # auth/authenticate.py
 
 # authenticate 의존 라이브러리가 포함되며 인증 및 권한을 위해 라우트에 주입된다.
+# 이 함수는 활성 세션에 존재하는 사용자 정보를 추출하는 단일 창구 역할을 한다.
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from auth.jwt_handler import verify_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/signin")
+"""
+OAuth2PasswordBearer(tokenUrl) 클래스의 인스턴스 oauth2_scheme 변수는
+보안 로직이 존재한다는 것을 애플리케이션에 알려준다.
+token 문자열을 추출한다.
+tokenUrl은 token을 받는 로그인 url을 입력한다.
+"""
+
+async def authenticate(token: str = Depends(oauth2_scheme)) -> str:
+    """
+    authenticate() 함수는 토큰을 인수로 받는다. 토큰이 유효하면 토큰을 디코딩한 후 페이로드의 사용자 필드를
+    반환하고 유효하지 않으면 verify_access_token() 함수에 정의된 오류 메시지를 반환한다.
+    라우트에 보안 적용을 위한 의존 라이브러리를 만들었다.
+    """
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Sign in for access"
+        )
+
+    decoded_token = verify_access_token(token)
+    return decoded_token["user"]
--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -26,3 +26,29 @@ def create_access_token(user: str):  # 토큰 생성 함수
 
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
     return token
+
+def verify_access_token(token: str):  # 토큰 검증 함수
+    try:
+        data = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        """
+        토큰을 디코딩해서 payload를 data변수로 받는다.
+        """
+        expire = data.get("expires")  # 딕셔너리의 get(x) 함수는 x라는 key에 대응되는 value값을 돌려준다.
+
+        if expire is None:  # 만료 시간이 없다면 유효한 토큰이 존재하지 않음
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No access token supplied"
+            )
+        if datetime.utcnow() > datetime.utcfromtimestamp(expire):  # 토큰이 유효한지(만료 시간이 지나지 않았는지)
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Token expired!"
+            )
+        return data
+    
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid token"
+        )
