fec0d20133cda7ab015eda3a171a1b34150556de
koreanjys <connecttojys@gmail.com>
2024-01-30 22:18:19 +0900

    error test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,18 @@
+# tests/test_login.py
+"""
+경고문구와 에러1개 발생
+에러는 이벤트 루프가 닫혔다는 에러인데 왜 발생하는지 원인 파악 필요..
+"""
+
 import httpx
 import pytest
 
 @pytest.mark.asyncio
 async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
     payload = {
-        "email": "testuser@packt.com",
+        "email": "testuser3@packt.com",
         "password": "testpassword",
-        "username": "packt"
+        "username": "packt3"
     }
     headers = {
         "accept": "application/json",
